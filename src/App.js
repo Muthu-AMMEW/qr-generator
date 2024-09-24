12d6a99629d76c2d3c997449200f3b7db037a7ec
Muthu-AMMEW <muthu.ammew@gmail.com>
2024-09-24 19:58:48 +0530

    update

--- src/App.js
+++ src/App.js
@@ -42,22 +42,22 @@ function App() {
         <div className='d-flex flex-column h-100 w-100 align-items-center justify-content-center mt-5'>
           <h1 className='text-success'>QR CODE GENERATOR</h1>
 
-          <div className='w-100 m-2 text-center'>
+          <div className='w-75 m-2 text-center'>
             {img && <img src={img} alt="" />}
             {loading && <p>loading...</p>}
           </div>
 
-          <div className='w-100 m-2'>
+          <div className='w-75 m-2'>
             <label className='form-label text-success' htmlFor="qrData">Data for QR Code</label> <br />
             <input className='form-control' type="text" name="qrData" id="qrData" value={qrData} onChange={(e) => setQrData(e.target.value)} />
           </div>
 
-          <div className='w-100 m-2'>
+          <div className='w-75 m-2'>
             <label className='form-label text-success' htmlFor="qrSize">Image size (e.g. 150)</label> <br />
             <input className='form-control' type="text" name="qrSize" id="qrSize" value={qrSize} onChange={(e) => setQrSize(e.target.value)} />
           </div>
 
-          <div className='w-100 mt-2 d-flex justify-content-between'>
+          <div className='w-75 mt-2 d-flex justify-content-between'>
             <button className='btn btn-warning' type="button" onClick={qrGenerate} disabled={loading}>Generate QR Code</button>
             <button className='btn btn-success ms-2' type="button" onClick={qrDownload}>Download QR Code</button>
           </div>
