2da27e7784ca90b51ee8fd9ceaf1e5908c08dded
Muthu-AMMEW <muthu.ammew@gmail.com>
2024-09-24 16:37:07 +0530

    React Finished

--- src/App.js
+++ src/App.js
@@ -1,33 +1,68 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [img, setImg] = useState("picture/demoQR.png");
+  const [loading, setLoading] = useState(false);
+  const [qrData, setQrData] = useState();
+  const [qrSize, setQrSize] = useState();
+
+  function qrGenerate() {
+    setLoading(true);
+    try {
+      const url = `https://api.qrserver.com/v1/create-qr-code/?size=${qrSize}x${qrSize}&data=${encodeURIComponent(qrData)}`;
+      setImg(url);
+    } catch (err) {
+      console.log("Error generating QR code", err)
+    } finally {
+      setLoading(false)
+    }
+
+  }
+  
+  function qrDownload() {
+    fetch(img).then((response) => response.blob())
+      .then((blob) => {
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = 'qrcode.png';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+      }).catch((err) => {
+        console.error("Error downloading QR code", err)
+      })
+  }
+
   return (
     <>
-      <div className=' container-md'>
-        <div className=' d-flex flex-column h-100 w-100 align-items-center justify-content-center mt-5'>
-          <h1 className=' text-success'>QR CODE GENERATOR</h1>
+      <div className='container-md'>
+        <div className='d-flex flex-column h-100 w-100 align-items-center justify-content-center mt-5'>
+          <h1 className='text-success'>QR CODE GENERATOR</h1>
 
-          <div className=' w-75 m-2'>
-            <img src="" alt="" />
+          <div className='w-100 m-2 text-center'>
+            {img && <img src={img} alt="" />}
+            {loading && <p>loading...</p>}
           </div>
 
-          <div className=' w-75 m-2'>
-            <label className=' form-label text-success' htmlFor="qrdata">Data for QR Code</label> <br />
-            <input className=' form-control' type="text" name="qrdata" id="qrdata" />
+          <div className='w-75 m-2'>
+            <label className='form-label text-success' htmlFor="qrData">Data for QR Code</label> <br />
+            <input className='form-control' type="text" name="qrData" id="qrData" value={qrData} onChange={(e) => setQrData(e.target.value)} />
           </div>
 
-          <div className=' w-75 m-2'>
-            <label className=' form-label text-success' htmlFor="qrsize">Image size (e.g. 150)</label> <br />
-            <input className=' form-control' type="text" name="qrsize" id="qrsize" />
+          <div className='w-75 m-2'>
+            <label className='form-label text-success' htmlFor="qrSize">Image size (e.g. 150)</label> <br />
+            <input className='form-control' type="text" name="qrSize" id="qrSize" value={qrSize} onChange={(e) => setQrSize(e.target.value)} />
           </div>
 
           <div className='w-75 mt-2 d-flex justify-content-between'>
-            <button className='btn btn-danger' type="button">Generate QR Code</button>
-            <button className='btn btn-success' type="button">Download QR Code</button>
+            <button className='btn btn-warning' type="button" onClick={qrGenerate} disabled={loading}>Generate QR Code</button>
+            <button className='btn btn-success' type="button" onClick={qrDownload}>Download QR Code</button>
           </div>
 
-          <div className='mt-3'><p>Designed By <a href="">Muthu</a></p></div>
+          <div className='mt-3'><p className=' fst-italic'>Designed By <a href="https://www.linkedin.com/in/muthu-ammew/">Muthu</a></p></div>
 
         </div>
       </div>
